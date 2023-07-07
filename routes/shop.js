f3058e503c09b4eaa542436ac9d6228a3a519c7b
fakhriyya rahimova <rahimovafakhriyya@gmail.com>
2023-07-07 13:01:56 +0200

    adding product, using engine pug

---
+++ routes/shop.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+const adminData = require('./admin');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+ const product = adminData.products;
+ res.render('shop', {prods: product, title: 'shop'})
+});
+
+module.exports = router;
